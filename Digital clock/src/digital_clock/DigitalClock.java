4f9b6b361c8daad982bf3dc805803d5a300a8ebf
KostaLaz <44593214+KostaLaz@users.noreply.github.com>
2019-03-26 17:06:48 +0100

    Digital clock fix

--- Digital clock/src/digital_clock/DigitalClock.java
+++ Digital clock/src/digital_clock/DigitalClock.java
@@ -1,21 +1,18 @@
 package digital_clock;
+
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Color;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-
 public class DigitalClock extends JFrame {
-	public DigitalClock() {
-	}
-	
+
 	JLabel jlabClock;
 	ClockThread ct;
-	
-	
-	
-	public void digitalClock () {
+
+	public DigitalClock() {
+
 		jlabClock = new JLabel("");
 		getContentPane().setLayout(new FlowLayout());
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -24,16 +21,13 @@ public class DigitalClock extends JFrame {
 		setLocationRelativeTo(null);
 		ct = new ClockThread(this);
 		setVisible(true);
-		
-		
-		
+
 	}
-	
 
 	public static void main(String[] args) {
 
 		new DigitalClock();
-		
+
 	}
 
 }
