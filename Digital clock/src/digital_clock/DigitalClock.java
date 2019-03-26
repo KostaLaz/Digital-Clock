a3332dfde60a3b987bf71bc26babb1d6d16c49b8
KostaLaz <44593214+KostaLaz@users.noreply.github.com>
2019-03-26 16:36:11 +0100

    Second commit

--- Digital clock/src/digital_clock/DigitalClock.java
+++ Digital clock/src/digital_clock/DigitalClock.java
@@ -6,11 +6,30 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 
-public class DigitalClock {
+public class DigitalClock extends JFrame {
+	public DigitalClock() {
+	}
+	
+	JLabel jlabClock;
+	
+	public void digitalClock () {
+		jlabClock = new JLabel("");
+		getContentPane().setLayout(new FlowLayout());
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().add(jlabClock);
+		setSize(1200, 120);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		
+		
+		
+	}
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		new DigitalClock();
+		
 	}
 
 }
