fea953d0df2e1012a5ac4679130661e08466e918
KostaLaz <44593214+KostaLaz@users.noreply.github.com>
2019-03-26 16:21:48 +0100

    First commit

---
+++ Digital clock/src/digital_clock/DigitalClock.java
@@ -0,0 +1,16 @@
+package digital_clock;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+
+public class DigitalClock {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
